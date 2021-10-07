86c6fd36ac4561f6913e489861cbaecf2503d37d
SkillFi <bimbom2345@gmail.com>
2021-10-07 11:05:08 +0300

    README.md

---
+++ Лабораторні/Лабораторна 3/main.py
@@ -0,0 +1,32 @@
+import dlib
+from skimage import io
+from scipy.spatial import distance
+sp = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+detector = dlib.get_frontal_face_detector()
+img = io.imread('AS1.png')
+win1 = dlib.image_window()
+win1.clear_overlay()
+win1.set_image(img)
+dets = detector(img, 1)
+for k, d in enumerate(dets):
+    print(f"Detection {k}: Left: {d.left()} Top: {d.top()} Right: {d.right()} Bottom: {d.bottom()}")
+    shape = sp(img, d)
+    win1.clear_overlay()
+    win1.add_overlay(d)
+    win1.add_overlay(shape)
+face_descriptor = facerec.compute_face_descriptor(img, shape)
+img = io.imread('AS2.jpg')
+win2 = dlib.image_window()
+win2.clear_overlay()
+win2.set_image(img)
+dets = detector(img, 1)
+for k, d in enumerate(dets):
+    print(f"Detection {k}: Left: {d.left()} Top: {d.top()} Right: {d.right()} Bottom: {d.bottom()}")
+    shape = sp(img, d)
+    win2.clear_overlay()
+    win2.add_overlay(d)
+    win2.add_overlay(shape)
+face_descriptor2 = facerec.compute_face_descriptor(img, shape)
+a = distance.euclidean(face_descriptor, face_descriptor2)
+print(a)
